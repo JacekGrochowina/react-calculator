3be5ed4fca51d637d30d668d23f24836d3ecd194
Jacek Grochowina <jacek99@vp.pl>
2020-01-19 14:57:23 +0100

    create structure

---
+++ src/layouts/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+//import '../css/main.css';
+import '../sass/main.scss';
+
+function App() {
+    return (
+        <div className="app">
+
+        </div>
+    );
+}
+
+export default App;
