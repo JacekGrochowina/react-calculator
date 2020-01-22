ed07e37ba1ea85dcfbbb805186900063ac808daf
Jacek Grochowina <jacek99@vp.pl>
2020-01-22 21:05:04 +0100

    add url adress

--- src/layouts/Footer.js
+++ src/layouts/Footer.js
@@ -4,7 +4,7 @@ const Footer = () => {
     return (
         <header className="app__footer footer">
             <span className="footer__span footer__span--break">Copyright 2020 &copy; All right reserved.</span>
-            <span className="footer__span">Designed by: <a className="footer__link" href="#">jacekcv.com</a></span>
+            <span className="footer__span">Designed by: <a className="footer__link" href="http://jacekcv.com/" target="_blank" rel="noopener noreferrer">jacekcv.com</a></span>
         </header>
     );
 }
