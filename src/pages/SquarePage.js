3f428d128472c2ae6c94c71746195037c2b0d042
Jacek Grochowina <jacek99@vp.pl>
2020-01-22 21:07:28 +0100

    create Class SquarePage, add state and input, import Calculator component

--- src/pages/SquarePage.js
+++ src/pages/SquarePage.js
@@ -1,19 +1,54 @@
-import React from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-
+import React, { Component } from 'react';
 import ButtonWraper from '../components/ButtonWraper';
+import Calculator from '../components/Calculator';
 
-const SquarePage = () => {
-    return (
-        <div className="square subpage">
-            <ButtonWraper path="/" text="wstecz" type="back" />
 
-            <section className="subpage__section">
-                <h1>Kwadrat</h1>
-            </section>
+class SquarePage extends Component {
 
-        </div>
-    )
+    state = {
+        number1: undefined,
+        number2: undefined,
+    }
+
+    handleOnChange = (e) => {
+        this.setState({
+            [e.target.name]: e.target.value,
+        })
+    }
+
+    render() {
+        return (
+            <div className="square subpage" >
+
+                <ButtonWraper path="/" text="wstecz" type="back" />
+
+                <section className="subpage__section">
+
+                    {/* Input */}
+                    <div className="input-wraper">
+                        <h3 className="input__h3">Podaj a</h3>
+                        <div className="row">
+                            <h3 className="input__h3--var">a =</h3>
+                            <input className="input" type="number" name="number1" value={this.state.number1} onChange={this.handleOnChange} />
+                        </div>
+                    </div>
+
+                    {/* Input */}
+                    <div className="input-wraper">
+                        <h3 className="input__h3">Podaj b</h3>
+                        <div className="row">
+                            <h3 className="input__h3--var">b =</h3>
+                            <input className="input" type="number" name="number2" value={this.state.number2} onChange={this.handleOnChange} />
+                        </div>
+                    </div>
+
+                    <Calculator name={"square"} a={Number(this.state.number1)} b={Number(this.state.number2)} />
+                </section>
+
+            </div>
+        );
+    }
+
 }
 
 export default SquarePage;
