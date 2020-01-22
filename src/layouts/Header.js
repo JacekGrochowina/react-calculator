69f1463fa59696b8401893987a19f6fc2564ca34
Jacek Grochowina <jacek99@vp.pl>
2020-01-22 21:05:37 +0100

    fix

--- src/layouts/Header.js
+++ src/layouts/Header.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Header = () => {
     return (
         <header className="app__header header">
-            <h1 class="header__h1">Kalkulator figur</h1>
+            <h1 className="header__h1">Kalkulator figur</h1>
         </header>
     );
 }
